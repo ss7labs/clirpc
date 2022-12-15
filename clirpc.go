0ded95580605be8f244326bfd2faa724d4073562
berk131 <berk131@gmail.com>
2022-12-15 14:58:14 +0500

    rpc acceppt fork tested

--- clirpc.go
+++ clirpc.go
@@ -3,3 +3,22 @@ package clirpc
 import (
 	_ "fmt"
 )
+
+type RawSession struct {
+	vif_id,
+	username,
+	mac,
+	port,
+	svid,
+	cvid,
+	session_id,
+	ip_addr,
+	mtu,
+	ingress_cir,
+	egress_cir,
+	rx_pkts,
+	tx_pkts,
+	rx_bytes,
+	tx_bytes,
+	uptime string
+}
--- clirpc_test.go
+++ clirpc_test.go
@@ -3,7 +3,7 @@ package clirpc
 import (
 	"fmt"
 	"log"
-  "net"
+	"net"
 	"net/rpc"
 	"testing"
 	"time"
@@ -12,6 +12,13 @@ import (
 const (
 	srvAddr = "127.0.0.1:58085"
 )
+
+var testData [2]RawSession
+
+func initTestData() {
+	testData[0] = RawSession{"25330868", "737155@gts", "7C:8B:CA:E2:DB:BE", "1", "3065", "1031", "1669976882-25330868", "172.17.5.62", "1492", "8M;10M", "2M;15M", "62299192", "56293332", "22641605997", "40947477724", "10 day(s), 5 hour(s), 46 min(s), 16 sec(s)"}
+	testData[1] = RawSession{"37905591", "430147@gts", "84:A9:C4:F9:56:A3", "1", "3131", "1061", "1670854858-37905591", "172.17.40.29", "1480", "-", "-", "0", "0", "0", "0", "0 day(s), 1 hour(s), 53 min(s), 20 sec(s)"}
+}
 
 type Listener struct {
 	Sleep time.Duration
@@ -22,10 +29,10 @@ func (l *Listener) GetUser(line []byte, ack *bool) (err error) {
 	return
 }
 
-func startServer(stop,started chan bool) {
+func startServer(stop, started chan bool) {
 	fmt.Println("Started server")
 
-  addr, err := net.ResolveTCPAddr("tcp", srvAddr)
+	addr, err := net.ResolveTCPAddr("tcp", srvAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,16 +46,16 @@ func startServer(stop,started chan bool) {
 	rpc.Register(listener)
 	fmt.Println("server")
 	go rpc.Accept(inbound)
-  started<-true
-  <-stop
+	started <- true
+	<-stop
 }
 
 func TestServer(t *testing.T) {
 	var stopSrv chan bool
 	stopSrv = make(chan bool)
-  srvStarted := make(chan bool)
+	srvStarted := make(chan bool)
 	go startServer(stopSrv, srvStarted)
-  <-srvStarted
+	<-srvStarted
 
 	client, err := rpc.Dial("tcp", srvAddr)
 	if err != nil {
